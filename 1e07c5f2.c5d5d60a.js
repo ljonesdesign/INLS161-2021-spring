a1743daabfdfec43d3632a219cb1eb317c9efac5
lblakej <ljones@nc.edu>
2021-01-27 14:34:54 -0500

    Deploy website - based on 8600133397a20ffab37cc2952b0e67413ae9acbe

---
+++ 1e07c5f2.c5d5d60a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{74:function(o){o.exports=JSON.parse('{"permalink":"/INLS161-2021-spring/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
